4d2c0bc6113d557fd39ed45b55422bb0c29e8bb2
Frank Ponte <fponte@auction.com>
2024-04-01 22:35:54 -0400

    portalasig-commons initial commit

---
+++ src/test/java/com/portalsig/portalasigcommons/PortalasigCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portalsig.portalasigcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalasigCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
